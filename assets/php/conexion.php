8ee746b4a318f4e35c37534bbdbb7a58eb7e8f92
GerardoCanoZamarripa <gcanozamarripa@gmail.com>
2022-07-02 23:45:41 -0500

    Se actualizo la forma de conectar a la base de datos a PDO

--- assets/php/conexion.php
+++ assets/php/conexion.php
@@ -1,6 +1,6 @@
 <?php
 
-$dsn = 'mysql:host=localhost;port=3306;dbname=second_life';
+$dsn = 'mysql:host=localhost;port=3306;dbname=secondlife';
 $username = 'root';
 $password = '';
 $options = [];
