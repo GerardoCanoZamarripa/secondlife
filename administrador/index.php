16cddf68e78fba2ec8fe5b5c622a1a7ef2b1d665
JoselinMV <joselinmv757@gmail.com>
2022-07-05 08:19:03 -0500

    Se agrego el index del administrador

---
+++ administrador/index.php
@@ -0,0 +1,235 @@
+<!doctype html>
+<html class="no-js" lang="es" xmlns:mso="urn:schemas-microsoft-com:office:office" xmlns:msdt="uuid:C2F41010-65B3-11d1-A29F-00AA00C14882">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Second Life</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="manifest" href="../site.webmanifest">
+    <link rel="shortcut icon" type="image/x-icon" href="../assets/img/favicon.ico">
+
+    <!-- CSS here -->
+    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="../assets/css/slicknav.css">
+    <link rel="stylesheet" href="../assets/css/flaticon.css">
+    <link rel="stylesheet" href="../assets/css/progressbar_barfiller.css">
+    <link rel="stylesheet" href="../assets/css/lightslider.min.css">
+    <link rel="stylesheet" href="../assets/css/price_rangs.css">
+    <link rel="stylesheet" href="../assets/css/gijgo.css">
+    <link rel="stylesheet" href="../assets/css/animate.min.css">
+    <link rel="stylesheet" href="../assets/css/animated-headline.css">
+    <link rel="stylesheet" href="../assets/css/magnific-popup.css">
+    <link rel="stylesheet" href="../assets/css/fontawesome-all.min.css">
+    <link rel="stylesheet" href="../assets/css/themify-icons.css">
+    <link rel="stylesheet" href="../assets/css/slick.css">
+    <link rel="stylesheet" href="../assets/css/nice-select.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
+    
+
+<!--[if gte mso 9]><xml>
+<mso:CustomDocumentProperties>
+<mso:display_urn_x003a_schemas-microsoft-com_x003a_office_x003a_office_x0023_SharedWithUsers msdt:dt="string">ADMON PROYECTOS Members</mso:display_urn_x003a_schemas-microsoft-com_x003a_office_x003a_office_x0023_SharedWithUsers>
+<mso:SharedWithUsers msdt:dt="string">22;#ADMON PROYECTOS Members</mso:SharedWithUsers>
+</mso:CustomDocumentProperties>
+</xml><![endif]-->
+</head>
+<body>
+    <!-- ? Preloader Start -->
+    <div id="preloader-active">
+        <div class="preloader d-flex align-items-center justify-content-center">
+            <div class="preloader-inner position-relative">
+                <div class="preloader-circle"></div>
+                <div class="preloader-img pere-text">
+                    <img src="../assets/img/logo/loder.png" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Preloader Start-->
+    <header>
+        <!-- Header Start -->
+        <div class="header-area">
+            <div class="main-header header-sticky">
+                <div class="container-fluid">
+                    <div class="row menu-wrapper align-items-center justify-content-between">
+                        <div class="header-left d-flex align-items-center">
+                            <!-- Logo -->
+                            <div class="logo">
+                                <a href="../index.html"><img src="../assets/img/logo/logo.png" alt=""></a>
+                            </div>
+                            <!-- Logo-2 -->
+                            <div class="logo2">
+                                <a href="../index.html"><img src="../assets/img/logo/logo2.png" alt=""></a>
+                            </div>
+                             <!-- Main-menu -->
+
+                        <?php include 'menuAdministrador.html'; ?>
+
+</div>
+
+<div class="header-right1 d-flex align-items-center">
+
+    <div class="search">
+
+        <ul class="d-flex align-items-center">
+
+            <li>
+
+                <a href="#" class="account-btn" target="">Juan Fernando Peréz Del Corral
+
+                <a href="../assets/php/cerrarSesion.php" class="account-btn" target="">Cerrar Sesión </a>
+
+                </a>
+
+            </li>
+
+        </ul>
+
+    </div>
+
+</div>
+                        <!-- Mobile Menu -->
+                        <div class="col-12">
+                            <div class="mobile_menu d-block d-lg-none"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Header End -->
+    </header>
+    <!-- header end -->
+    <main>
+        <!--? slider Area Start-->
+        <div class="slider-area ">
+            <div class="slider-active">
+                <div class="single-slider hero-overly1 slider-height d-flex align-items-center slider-bg1">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-xl-6 col-lg-8 col-md-8">
+                                <div class="hero__caption">
+                                    <h1 data-animation="fadeInUp" data-delay=".4s">Productos Second Life</h1>
+                                    <p data-animation="fadeInUp" data-delay=".6s">Donde la vida esta llena de segundas oportunidades.</p>  
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- slider Area End-->
+        
+        <!--? Properties Start -->
+        <section class="properties new-arrival fix">
+            <div class="container">
+                <!-- Section tittle -->
+                <div class="row justify-content-center">
+                    <div class="col-xl-7 col-lg-8 col-md-10">
+                        <div class="section-tittle mb-60 text-center wow fadeInUp" data-wow-duration="1s" data-wow-delay=".2s">
+                            <h2>¿Quiénes somos?</h2>
+                            <P>Second Life surge a partir de observar como las personas buscan intercambiar distintos productos de segunda mano en plataformas que no están destinadas a esta actividad, por ello diseñamos un medio que permite a los usuarios comprar y vender de manera sencilla.</P>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Properties End -->
+        <!--? Visit Our Tailor Start -->
+        <div class="visit-tailor-area fix">
+            <!--Right Contents  -->
+            <div class="tailor-offers1"></div>
+            <!-- left Contents -->
+            <div class="tailor-details">
+                <h2>Misión</h2>
+                <p>Somos una empresa e-commerce dedicada a unir a vendedores y compradores en un sitio que agiliza las transacciones entre ambos, buscando siempre la seguridad de todos nuestros usuarios.</p>
+                <h2>Visión</h2>
+                <p>Ser la empresa líder en e-commerce, reconocida a nivel nacional por todas aquellas personas que busquen un método sencillo, eficaz de compra y venta de artículos de segunda mano manteniendo siempre la seguridad en cada compra que caracteriza a “Second Life”.</p>
+                
+            </div>
+
+        </div>
+        <!-- Visit Our Tailor End -->
+        <!--? New Arrival-2 Start -->
+        <div class="new-arrival new-arrival2">
+            <div class="container">
+                <!-- Section tittle -->
+                <div class="row justify-content-center">
+                    <div class="col-xl-6 col-lg-8 col-md-10">
+                        <div class="section-tittle mb-60 text-center wow fadeInUp" data-wow-duration="2s" data-wow-delay=".2s">
+                            <h2>Valores</h2>
+                            <P>Competitividad: Somos una empresa que conoce lo importante que es avanzar tomando los riesgos necesarios.</P>
+                            <p>Trabajo en equipo: Se conocen la importancia de fomentar el trabajo en equipo.</p>
+                            <p>Excelencia: Se busca ejecutar tareas con los máximos estándares de excelencia</p>
+                            <p>Transparencia: Somos una empresa transparente, honesta y accesible.</p>
+                            <p>Fidelización: Se ofrece a los consumidores de la tienda online recibir un trato exclusivo y personalizado que les recompense con ofertas, promociones y descuentos.</p>
+                            <p>Accesibilidad: La empresa ofrece una accesibilidad desde cualquier dispositivo y estar disponible a cualquier hora y desde cualquier lugar. </p>
+                        </div>
+                    </div>
+                </div>
+               
+                
+
+         </div>
+     </div>
+ </div>
+ <!--? New Arrival End -->
+ <!-- Popular Locations End -->
+ <!--? Services Area Start -->
+
+<!--? Services Area End -->
+</main>
+<footer>
+ <?php include '../footer.html'; ?>
+ </footer>
+<!-- Scroll Up -->
+<div id="back-top" >
+    <a title="Go to Top" href="#"> <i class="fas fa-level-up-alt"></i></a>
+</div>
+
+<!-- JS here -->
+<!-- Jquery, Popper, Bootstrap -->
+<script src="../assets/js/vendor/modernizr-3.5.0.min.js"></script>
+<script src="../assets/js/vendor/jquery-1.12.4.min.js"></script>
+<script src="../assets/js/popper.min.js"></script>
+<script src="../assets/js/bootstrap.min.js"></script>
+
+<!-- Slick-slider , Owl-Carousel ,slick-nav -->
+<script src="../assets/js/owl.carousel.min.js"></script>
+<script src="../assets/js/slick.min.js"></script>
+<script src="../assets/js/jquery.slicknav.min.js"></script>
+
+<!-- One Page, Animated-HeadLin, Date Picker , price, light-slider -->
+<script src="../assets/js/wow.min.js"></script>
+<script src="../assets/js/animated.headline.js"></script>
+<script src="../assets/js/jquery.magnific-popup.js"></script>
+<script src="../assets/js/gijgo.min.js"></script>
+<script src="../assets/js/lightslider.min.js"></script>
+<script src="../assets/js/price_rangs.js"></script>
+
+<!-- Nice-select, sticky,Progress -->
+<script src="../assets/js/jquery.nice-select.min.js"></script>
+<script src="../assets/js/jquery.sticky.js"></script>
+<script src="../assets/js/jquery.barfiller.js"></script>
+
+<!-- counter , waypoint,Hover Direction -->
+<script src="../assets/js/jquery.counterup.min.js"></script>
+<script src="../assets/js/waypoints.min.js"></script>
+<script src="../assets/js/jquery.countdown.min.js"></script>
+<script src="../assets/js/hover-direction-snake.min.js"></script>
+
+<!-- contact js -->
+<script src="../assets/js/contact.js"></script>
+<script src="../assets/js/jquery.form.js"></script>
+<script src="../assets/js/jquery.validate.min.js"></script>
+<script src="../assets/js/mail-script.js"></script>
+<script src="../assets/js/jquery.ajaxchimp.min.js"></script>
+
+<!-- Jquery Plugins, main Jquery -->	
+<script src="../assets/js/plugins.js"></script>
+<script src="../assets/js/main.js"></script>
+
+</body>
+</html>
